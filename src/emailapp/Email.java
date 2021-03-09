21e4e71f7d14abd568385242c29ee54a7dc6340a
ken025 <kennethmanuel250@gmail.com>
2021-03-09 11:14:49 -0500

    added method to ask for department

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,11 +1,47 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class  Email {
 //    properties
-    private String fistName;
+    private String firstName;
     private String lastName;
     private String password;
-    private String deparment;
+    private String department;
     private int mailBoxCapacity;
     private String alternateEmail;
+
+//    constructor to receive first and last name
+    public Email(String firstName, String lastName){
+//  'this' refers to the class level variable
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: " + firstName + " " + lastName );
+
+        this.department = setDept();
+        System.out.println("Department: " + this.department);
+    }
+
+//    ask for department
+    private String setDept(){
+        System.out.print("\n1 for Sales\n2 for Developmnet\n3 for Accounting\n0 for none\n Enter the Department Number: ");
+        Scanner in = new Scanner(System.in);
+        int deptChoice = in.nextInt();
+        if (deptChoice == 1){
+            return "Sales";
+        } else if (deptChoice == 2){
+            return "Dev";
+        } else if (deptChoice == 3){
+            return "Accounting";
+        } else {
+            return "";
+        }
+    }
+//    generate random password
+
+//    set the mailbox capacity
+
+//    set the alternate email
+
+//    change password
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -3,6 +3,6 @@ package emailapp;
 public class EmailApp {
 
     public static void main(String[] args){
-
+        Email eml1 = new Email("John", "Smith");
     }
 }
