e0f991561fecc16173cd5e7b96bf5f5bba68763e
ken025 <kennethmanuel250@gmail.com>
2021-03-08 23:25:33 -0500

    Added classes amd properties

---
+++ src/emailapp/Email.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class  Email {
+//    properties
+    private String fistName;
+    private String lastName;
+    private String password;
+    private String deparment;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+}
